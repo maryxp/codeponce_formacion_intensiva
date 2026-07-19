d048cb8a8c91f4d2a9265c61bc59d07304f5a175
maria laura <mary.cool2707@gmail>
2026-07-18 21:23:00 -0400

    button: Implementé dark mode

--- clase_03/script.js
+++ clase_03/script.js
@@ -1,3 +1,8 @@
 document.getElementById('btnSaludar').addEventListener('click', function() {
     document.getElementById('mensaje').textContent = '¡Hola! Gracias por hacer clic.';
 });
+
+document.getElementById('btnDarkMode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Oscuro';
+});
